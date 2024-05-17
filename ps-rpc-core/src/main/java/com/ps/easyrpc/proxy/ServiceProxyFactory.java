08a4d1b1459f22415ceff91c637bdd94ccdd24d2
pangshuai12138 <424026443@qq.com>
2024-05-17 20:40:38 +0800

    feat: 新增接口Mock功能

--- ps-rpc-core/src/main/java/com/ps/easyrpc/proxy/ServiceProxyFactory.java
+++ ps-rpc-core/src/main/java/com/ps/easyrpc/proxy/ServiceProxyFactory.java
@@ -1,5 +1,7 @@
 package com.ps.easyrpc.proxy;
 
+import com.ps.easyrpc.RpcApplication;
+
 import java.lang.reflect.Proxy;
 
 /**
@@ -17,9 +19,30 @@ public class ServiceProxyFactory {
      * @return
      */
     public static <T> T getProxy(Class<T> serviceClass) {
+        // 是否mock
+        if(RpcApplication.getRpcConfig().isMock()){
+            return getMockProxy(serviceClass);
+        }
+
         return (T) Proxy.newProxyInstance(
                 serviceClass.getClassLoader(),
                 new Class[]{serviceClass},
                 new ServiceProxy());
+    }
+
+    /**
+     * 根据服务类获取 Mock 代理对象
+     *
+     * @param serviceClass
+     * @param <T>
+     * @return
+     */
+    public static <T> T getMockProxy(Class<T> serviceClass) {
+
+        return (T) Proxy.newProxyInstance(
+                serviceClass.getClassLoader(),
+                new Class[]{serviceClass},
+                // Mock 服务代理
+                new MockServiceProxy());
     }
 }
