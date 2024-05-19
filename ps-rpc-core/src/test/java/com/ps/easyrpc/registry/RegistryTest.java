86c38bb2cf5acf3ec0394e01090bd73abb6bb194
pangshuai12138 <424026443@qq.com>
2024-05-19 15:49:49 +0800

    feat:注册中心测试

---
+++ ps-rpc-core/src/test/java/com/ps/easyrpc/registry/RegistryTest.java
@@ -0,0 +1,64 @@
+package com.ps.easyrpc.registry;
+
+import com.ps.easyrpc.config.RegistryConfig;
+import com.ps.easyrpc.model.ServiceMetaInfo;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * 注册中心测试
+ */
+public class RegistryTest {
+
+    final Registry registry = new EtcdRegistry();
+
+    @Before
+    public void init() {
+        RegistryConfig registryConfig = new RegistryConfig();
+        registryConfig.setAddress("http://localhost:2379");
+        registry.init(registryConfig);
+    }
+
+
+    @Test
+    public void register() throws Exception {
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.0");
+        serviceMetaInfo.setServiceAddress("http://localhost:1234");
+        registry.register(serviceMetaInfo);
+        serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.0");
+        serviceMetaInfo.setServiceAddress("http://localhost:1235");
+        registry.register(serviceMetaInfo);
+        serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("2.0");
+        serviceMetaInfo.setServiceAddress("http://localhost:1234");
+        registry.register(serviceMetaInfo);
+    }
+
+    @Test
+    public void unRegister() {
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.0");
+        serviceMetaInfo.setServiceAddress("http://localhost:1234");
+        registry.unRegister(serviceMetaInfo);
+    }
+
+    @Test
+    public void serviceDiscovery() {
+        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+        serviceMetaInfo.setServiceName("myService");
+        serviceMetaInfo.setServiceVersion("1.0");
+        String serviceKey = serviceMetaInfo.getServiceKey();
+        List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceKey);
+        System.out.println(serviceMetaInfoList.size());
+        Assert.assertNotNull(serviceMetaInfoList);
+    }
+}
